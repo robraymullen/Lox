8dd46e77fb0df1be86f3198863f7e09b8617dd45
robraymullen <robraymullen@gmail.com>
2022-01-20 23:10:22 +0000

    Class declarations (#10)
    * Class declarations
    
    can now declare classes
    
    * Class properties
    Can now set and get properties on classes
    
    * This
    Adding functionality for instance referencing within method calls with the 'this' keyword
    
    * Constructors
    Added functionality for initializer methods for object construction

--- jlox/src/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -58,6 +58,24 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	}
 	
 	@Override
+	public Object visitSetExpr(Expr.Set expr) {
+		Object object = evaluate(expr.object);
+		
+		if (!(object instanceof LoxInstance)) {
+			throw new RuntimeError(expr.name, "Only instances have fields.");
+		}
+		
+		Object value = evaluate(expr.value);
+		((LoxInstance)object).set(expr.name, value);
+		return value;
+	}
+	
+	@Override
+	public Object visitThisExpr(Expr.This expr) {
+		return lookupVariable(expr.keyword, expr);
+	}
+	
+	@Override
 	public Object visitUnaryExpr(Expr.Unary expr) {
 		Object right = evaluate(expr.right);
 		
@@ -162,6 +180,21 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	}
 	
 	@Override
+	public Void visitClassStmt(Stmt.Class stmt) {
+		environment.define(stmt.name.lexeme, null);
+		
+		Map<String, LoxFunction> methods = new HashMap<>();
+		for (Stmt.Function method : stmt.methods) {
+			LoxFunction function = new LoxFunction(method, environment, method.name.lexeme.equals("init"));
+			methods.put(method.name.lexeme, function);
+		}
+		
+		LoxClass klass = new LoxClass(stmt.name.lexeme, methods);
+		environment.assign(stmt.name, klass);
+		return null;
+	}
+	
+	@Override
 	public Void visitExpressionStmt(Stmt.Expression stmt) {
 		evaluate(stmt.expression);
 		return null;
@@ -169,7 +202,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	
 	@Override
 	public Void visitFunctionStmt(Stmt.Function stmt) {
-		LoxFunction function = new LoxFunction(stmt, environment);
+		LoxFunction function = new LoxFunction(stmt, environment, false);
 		environment.define(stmt.name.lexeme, function);
 		return null;
 	}
@@ -310,6 +343,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 			throw new RuntimeError(expr.paren, "Expected "+ function.arity()+" arguments but got "+arguments.size()+".");
 		}
 		return function.call(this, arguments);
+	}
+	
+	@Override
+	public Object visitGetExpr(Expr.Get expr) {
+		Object object = evaluate(expr.object);
+		if (object instanceof LoxInstance) {
+			return ((LoxInstance) object).get(expr.name);
+		}
+		
+		throw new RuntimeError(expr.name, "Only instances have properties.");
 	}
 
 }
