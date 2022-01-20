4125dfcb38bf13d2e0308dcb497f5be272a75fb7
robraymullen <robraymullen@gmail.com>
2022-01-20 22:47:16 +0000

    This Adding functionality for instance referencing within method calls with the 'this' keyword

--- jlox/src/com/craftinginterpreters/lox/Resolver.java
+++ jlox/src/com/craftinginterpreters/lox/Resolver.java
@@ -24,8 +24,16 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 	
 	private enum FunctionType {
 		NONE,
-		FUNCTION
+		FUNCTION,
+		METHOD
 	}
+	
+	private enum ClassType {
+		NONE,
+		CLASS
+	}
+	
+	private ClassType currentClass = ClassType.NONE;
 	
 	@Override
 	public Void visitBlockStmt(Stmt.Block stmt) {
@@ -37,8 +45,22 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 	
 	@Override
 	public Void visitClassStmt(Stmt.Class stmt) {
+		ClassType enclosingClass = currentClass;
+		currentClass = ClassType.CLASS;
 		declare(stmt.name);
 		define(stmt.name);
+		
+		beginScope();
+		scopes.peek().put("this", true);
+		
+		for (Stmt.Function method : stmt.methods) {
+			FunctionType declaration = FunctionType.METHOD;
+			resolveFunction(method, declaration);
+		}
+		
+		endScope();
+		
+		currentClass = enclosingClass;
 		return null;
 	}
 	
@@ -158,6 +180,16 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 	public Void visitSetExpr(Expr.Set expr) {
 		resolve(expr.value);
 		resolve(expr.object);
+		return null;
+	}
+	
+	@Override
+	public Void visitThisExpr(Expr.This expr) {
+		if (currentClass == ClassType.NONE) {
+			Lox.error(expr.keyword, "Can't use 'this' outside of a class.");
+			return null;
+		}
+		resolveLocal(expr, expr.keyword);
 		return null;
 	}
 	
